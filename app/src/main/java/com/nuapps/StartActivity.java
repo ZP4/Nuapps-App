50a503b1079c28abe3a577e765982d024c145040
ZP4 <jasonhuzheming9@gmail.com>
2017-01-25 09:48:09 -0500

    Added Typewriter class

--- app/src/main/java/com/nuapps/StartActivity.java
+++ app/src/main/java/com/nuapps/StartActivity.java
@@ -1,5 +1,7 @@
 package com.nuapps;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        setTitle("");
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/SourceCodePro-Bold.ttf");
+
+        TypeWriter startActWriter = (TypeWriter)findViewById(R.id.Typewriter);
+        startActWriter.setCharacterDelay(175);
+        startActWriter.setTypeface(custom_font);
+        startActWriter.setTextColor(Color.BLACK);
+        startActWriter.animateText("Nuapps_");
+
     }
 }
---
+++ app/src/main/java/com/nuapps/TypeWriter.java
@@ -0,0 +1,53 @@
+package com.nuapps;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by ZP4 on 1/24/2017.
+ */
+
+public class TypeWriter extends TextView{
+    private CharSequence mText;
+    private int mIndex;
+    private long mDelay = 500; //Default 500ms delay
+
+
+    public TypeWriter(Context context) {
+        super(context);
+    }
+
+    public TypeWriter(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private Handler mHandler = new Handler();
+    private Runnable characterAdder = new Runnable() {
+        @Override
+        public void run() {
+            setText(mText.subSequence(0, mIndex++));
+            if(mIndex <= mText.length()) {
+                mHandler.postDelayed(characterAdder, mDelay);
+            }
+
+        }
+    };
+
+    public void animateText(CharSequence text) {
+        mText = text;
+        mIndex = 0;
+
+        setText("");
+        mHandler.removeCallbacks(characterAdder);
+        mHandler.postDelayed(characterAdder, mDelay);
+    }
+
+    public void setCharacterDelay(long millis) {
+        mDelay = millis;
+    }
+
+
+}
